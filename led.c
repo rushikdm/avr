5de5ccad2b35b2ff19188c70b9e445ae8b745681
Rushikesh Deshmukh <58228830+rushikdm@users.noreply.github.com>
2022-02-12 14:45:16 +0530

    Update led.c

--- led.c
+++ led.c
@@ -1,3 +1,8 @@
+/*
+avr-gcc -Wall -Os -mmcu=atmega8 -o led.o led.c 
+avr-objcopy -j .text -j .data -O ihex led.o led.hex 
+avrdude -p ATmega8 -c USBasp -P COM3 -b 19200 -U flash:w:led.hex:i 
+*/
 #define F_CPU 1000000UL
 #include <avr/io.h>
 #include <util/delay.h>

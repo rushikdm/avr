1e0a7173d65cea55f0239450b51f068f95737b0c
Rushikesh Deshmukh <58228830+rushikdm@users.noreply.github.com>
2022-02-13 17:16:46 +0530

    Update myavrutils.h

--- myavrutils.h
+++ myavrutils.h
@@ -3,7 +3,7 @@
 
 #define LOW 0
 #define HIGH 1
-
+/*
 #define _SetPinAsInput(ddr, pos) DDR##ddr &= ~(1 << DDR##ddr##pos)
 #define SetPinAsInput(pin) _SetPinAsInput(pin)
 
@@ -18,58 +18,57 @@
 
 #define _GetPinState(port, pos) ((PIN##port & (1 << P##port##pos)) ? HIGH : LOW)
 #define GetPinState(pin) _GetPinState(pin)
-
+*/
 enum ReleaseType
 {
-  NO_PRESS=0,
-  SIMPLE_PRESS,
-  LONG_PRESS
+	NO_PRESS=0,
+	SIMPLE_PRESS,
+	LONG_PRESS
 };
 
 class Button
 {
-  uint16_t press_count;
-  uint16_t release_count;
-  ReleaseType lastEvent;
+	uint16_t press_count;
+	uint16_t release_count;
+	ReleaseType lastEvent;
 	
-public:
-  Button()
-  {
-    press_count = 0;
-    release_count = 0;
-    lastEvent = NO_PRESS;
-  }
+	public:
+	Button()
+	{
+		press_count = 0;
+		release_count = 0;
+		lastEvent = NO_PRESS;
+	}
 	
-  ReleaseType process(const bool high)
-  {
-    if(high)
-    {
-      if(press_count < 10000)
-        ++press_count;
-      release_count = 0;
-    }
-    else
-    {
-      if(release_count < 10000)
-        ++release_count;
-      press_count = 0;
-    }
+	ReleaseType process(const bool high)
+	{
+		if(high)
+		{
+			if(press_count < 10000)
+				++press_count;
+			release_count = 0;
+		}
+		else
+		{
+			if(release_count < 10000)
+				++release_count;
+			press_count = 0;
+		}
 		
-    if(press_count > 400)
-      lastEvent = SIMPLE_PRESS;
-    if(press_count > 1000)
-      lastEvent = LONG_PRESS;
+		if(press_count > 400)
+			lastEvent = SIMPLE_PRESS;
+		if(press_count > 1000)
+			lastEvent = LONG_PRESS;
 
-    if(release_count > 200)
-    {
-      ReleaseType temp = lastEvent;
-      lastEvent = NO_PRESS;
-      return temp;
-    }
+		if(release_count > 200)
+		{
+			ReleaseType temp = lastEvent;
+			lastEvent = NO_PRESS;
+			return temp;
+		}
 		
-    return NO_PRESS;
-  }
+		return NO_PRESS;
+	}
 };
 
 #endif
-

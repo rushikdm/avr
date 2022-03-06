d2aec50b3836ed54e162989118a9f38894722192
Rushikesh Deshmukh <58228830+rushikdm@users.noreply.github.com>
2022-03-06 13:40:34 +0530

    Update Multi7SegDisplay.h

--- Multi7SegDisplay.h
+++ Multi7SegDisplay.h
@@ -28,45 +28,36 @@
       9   1   1   0   1   1   1   1   0x6F
 
 */
-
-#define SegmentA      C,5
-#define SegmentB      C,4
-#define SegmentC      C,3
-#define SegmentD      C,1
-#define SegmentE      C,0
-#define SegmentF      D,0
-#define SegmentG      D,1
+/*
+#define SegmentA      B,0
+#define SegmentB      D,0
+#define SegmentC      D,5
+#define SegmentD      D,6
+#define SegmentE      D,7
+#define SegmentF      D,1
+#define SegmentG      D,3
 
 #define Digit1Pin B,5
 #define Digit2Pin B,4
+#define Digit3Pin B,4
 
+*/
 
 class Multi7SegDisplay
 {
-  const uint8_t segData[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
-  uint8_t digits[3] = {3, 2, 1};
-  uint8_t digitMInd = 0;
-  uint8_t segMInd = 0;
-  uint8_t digitInd = 0;
+  uint8_t digits[3];
+  uint8_t digitMInd;
+  uint8_t digitInd;
                              //A   B   C   D   E   F  G
   const uint8_t segPins[7] = { 14, 2, 11, 12, 13, 3, 5};
   const uint8_t digitPins[3] = { 4, 6, 10};
-  
-  bool present(const uint8_t idigit, const uint8_t iseg)
-  {
-    if(idigit < 10 && iseg < 8)
-      if(segData[idigit] & (1 << iseg))
-        return true;
-    return false;
-  }
-  
+
+  const uint8_t port_B[10] = { 0x01, 0x00, 0x01, 0x01, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01};
+  const uint8_t port_D[10] = { 0xe3, 0x21, 0xc9, 0x69, 0x2b, 0x6a, 0xea, 0x21, 0xeb, 0x6b};
+    
 public:  
   
-  Multi7SegDisplay() 
-  {
-    digits[0] = 1;
-    digits[1] = 2;
-  }
+  Multi7SegDisplay() : digits{3,2,1}, digitMInd(0), digitInd(0) { }
 
   void Setup7SegmentPins()
   {
@@ -79,11 +70,11 @@ public:
     SetPinAsOutput(digitPins[2]);
     
     for(ind = 0; ind < 7; ++ind)
-      SetPinValueHigh(segPins[ind]);
+      SetPinValueLow(segPins[ind]);
     
-    SetPinValueLow(digitPins[0]);
-    SetPinValueLow(digitPins[1]);
-    SetPinValueLow(digitPins[2]);
+    SetPinValueHigh(digitPins[0]);
+    SetPinValueHigh(digitPins[1]);
+    SetPinValueHigh(digitPins[2]);
   }
  
   void ShiftDigit()
@@ -102,28 +93,33 @@ public:
   
   void Multiplex()
   {
-    const uint8_t prev_pin = segPins[segMInd > 0 ? (segMInd-1) : 6];
-    SetPinValueHigh(prev_pin);
+    DisplayDigit(digits[digitMInd]);
+    ++digitMInd;
+    if(digitMInd > 2)
+      digitMInd = 0;
+  }
+  
+  void DisplayDigit(const uint8_t digit)
+  {
+    if(digit > 9)
+      return;
+
+    const uint8_t prev_digit_pin = digitPins[digitMInd > 0 ? (digitMInd-1) : 2];
+    SetPinValueHigh(prev_digit_pin);
     
-    if(segMInd == 0)
-    {
-      const uint8_t prev_digit = digitPins[digitMInd > 0 ? (digitMInd-1) : 1];
-      SetPinValueLow(prev_digit);
-      
-      SetPinValueHigh(digitPins[digitMInd]);
-    }
+    PORTB = (PORTB & 0b11111110) | (0b00000001 & port_B[digit]);
+    PORTD = (PORTD & 0b00010100) | (0b11101011 & port_D[digit]);
     
-    if(present(digits[digitMInd], segMInd))
-      SetPinValueLow(segPins[segMInd]);
-    
-    ++segMInd;
-    if(segMInd > 6)
-    {
-      segMInd = 0;
-      ++digitMInd;
-      if(digitMInd > 2)
-        digitMInd = 0;
-    }
+    SetPinValueLow(digitPins[digitMInd]);
+  }
+  
+  void ClearDisplay()
+  {
+    SetPinValueHigh(digitPins[0]);
+    SetPinValueHigh(digitPins[1]);
+    SetPinValueHigh(digitPins[2]);
+    PORTB = 0x00;
+    PORTD = 0x00;
   }
   
   void SetDigit(const uint8_t index, const uint8_t value)

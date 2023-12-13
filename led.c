28a2d11cba801007719653abd2b09566452dd463
Rushikesh Deshmukh <58228830+rushikdm@users.noreply.github.com>
2023-12-13 16:13:10 +0530

    Update led.c

--- led.c
+++ led.c
@@ -1,4 +1,6 @@
 /*
+@author: Rushikesh Keshvaro Deshmukh
+@date : 13-Dec-2023
 avr-gcc -Wall -Os -mmcu=atmega8 -o led.o led.c 
 avr-objcopy -j .text -j .data -O ihex led.o led.hex 
 avrdude -p ATmega8 -c USBasp -P COM3 -b 19200 -U flash:w:led.hex:i 
